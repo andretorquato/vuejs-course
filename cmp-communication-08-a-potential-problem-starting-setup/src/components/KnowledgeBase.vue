006d78915697b1bb4459455571b45f89ccd650ef
André Torquato <andretorquato@alu.ufc.br>
2021-12-02 17:32:16 -0300

    vue providers

---
+++ cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeBase.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <h2>Select a Topic</h2>
+    <knowledge-grid :topics="topics"></knowledge-grid>
+  </section>
+</template>
+
+<script>
+export default {
+  inject: ['topics'],
+};
+</script>
