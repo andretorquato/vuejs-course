d11801562f05ff9e0c7506d35782d77100f01e27
André Torquato <andretorquato@alu.ufc.br>
2021-12-09 17:51:11 -0300

    vue teleport element feature

--- cmp-adv-01-starting-setup/src/components/ActiveGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ActiveGoals.vue
@@ -2,34 +2,36 @@
   <h1>Active Goals</h1>
   <input type="text" ref="goal" />
   <button @click="setGoal">set goal</button>
-  <error-message v-if="inputIsInvalid">
-    <h1>Input is invalid</h1>
-    <p>Please correct the field</p>
-    <button @click="confirmGoal">OK</button>
-  </error-message>
+  <teleport to="body">
+    <error-message v-if="inputIsInvalid">
+      <h1>Input is invalid</h1>
+      <p>Please correct the field</p>
+      <button @click="confirmGoal">OK</button>
+    </error-message>
+  </teleport>
 </template>
 
 <script>
-import ErrorMessage from './ErrorMessage.vue';
+import ErrorMessage from "./ErrorMessage.vue";
 export default {
   components: {
-    ErrorMessage
+    ErrorMessage,
   },
-  data(){
+  data() {
     return {
       inputIsInvalid: false,
-    }
+    };
   },
   methods: {
     setGoal() {
       const enteredValue = this.$refs.goal.value;
-      if(enteredValue == '') {
+      if (enteredValue == "") {
         this.inputIsInvalid = true;
       }
     },
     confirmGoal() {
       this.inputIsInvalid = false;
-    }
+    },
   },
 };
 </script>
