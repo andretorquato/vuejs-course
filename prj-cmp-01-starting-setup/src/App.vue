553815d002be19197a0d407b42280fefe8d7003c
André Torquato <andretorquato@alu.ufc.br>
2021-12-10 17:51:32 -0300

    new template

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storedResource"
+      :key="res.id"
+      :description="res.description"
+      :title="res.title"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resource/LearningResource.vue';
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'youtube-resource',
+          title: 'Youtube educational content',
+          description: 'This is a youtube resource',
+          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        },
+        {
+          id: 'google-learning',
+          title: 'Searching for Google Learning',
+          description: 'This is a google learning resource',
+          link: 'https://www.google.com/search?q=google+learning',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ prj-cmp-01-starting-setup/src/components/learning-resource/LearningResource.vue
@@ -0,0 +1,13 @@
+<template>
+  <li>
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+    <a :href="link" target="_blank">About more</a>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['title', 'description', 'link'],
+};
+</script>
