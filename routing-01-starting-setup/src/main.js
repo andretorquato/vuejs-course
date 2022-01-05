de9913fae0647ecd10a31634b2ce4a9a394af717
André Torquato <andretorquato@alu.ufc.br>
2022-01-04 22:58:30 -0300

    refactor routes

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -1,70 +1,8 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
-import TeamsList from './components/teams/TeamsList.vue';
-import TeamMembers from './components/teams/TeamMembers.vue';
-import UsersList from './components/users/UsersList.vue';
-import NotFound from './components/nav/NotFound.vue';
-import TeamsFooter from './components/teams/TeamsFooter.vue';
-import UsersFooter from './components/users/UsersFooter.vue';
+import router from './router';
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', redirect: '/teams' },
-    {
-      name: 'teams',
-      path: '/teams',
-      meta: { needsAuth: true},
-      components: { default: TeamsList, footer: TeamsFooter },
-      children: [
-        {
-          name: 'team-members',
-          path: '/teams/:teamId',
-          component: TeamMembers,
-          props: true,
-        },
-      ],
-    },
-    {
-      path: '/users',
-      components: { default: UsersList, footer: UsersFooter },
-      beforeEnter(to, from, next) {
-        console.log('beforeEnter');
-        next();
-      },
-    },
-
-    { path: '/:notFound(.*)', component: NotFound },
-  ],
-  linkActiveClass: 'active',
-  scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    }
-    return { left: 0, top: 0 };
-  },
-});
-
-router.beforeEach(function (to, from, next) {
-  console.log('Global BeforeEach');
-  if(to.meta.needsAuth){
-    console.log("Need authentication");
-
-  }
-  // if(to.name === 'team-members'){
-  //   next();
-  // } else {
-  //   next({ name: 'team-members', params: { teamId: 't2' } });
-  // }
-  next();
-});
-
-router.afterEach(function (to, from) {
-  console.log('Global AfterEach');
-  console.log(to, from);
-});
 const app = createApp(App);
 app.use(router);
 
---
+++ routing-01-starting-setup/src/router.js
@@ -0,0 +1,65 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import TeamsList from './pages/TeamsList.vue';
+import TeamMembers from './pages/TeamMembers.vue';
+import UsersList from './pages/UsersList.vue';
+import NotFound from './pages/NotFound.vue';
+import TeamsFooter from './pages/TeamsFooter.vue';
+import UsersFooter from './pages/UsersFooter.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' },
+    {
+      name: 'teams',
+      path: '/teams',
+      meta: { needsAuth: true },
+      components: { default: TeamsList, footer: TeamsFooter },
+      children: [
+        {
+          name: 'team-members',
+          path: '/teams/:teamId',
+          component: TeamMembers,
+          props: true,
+        },
+      ],
+    },
+    {
+      path: '/users',
+      components: { default: UsersList, footer: UsersFooter },
+      beforeEnter(to, from, next) {
+        console.log('beforeEnter');
+        next();
+      },
+    },
+
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+  linkActiveClass: 'active',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  },
+});
+
+router.beforeEach(function (to, from, next) {
+  console.log('Global BeforeEach');
+  if (to.meta.needsAuth) {
+    console.log('Need authentication');
+  }
+  // if(to.name === 'team-members'){
+  //   next();
+  // } else {
+  //   next({ name: 'team-members', params: { teamId: 't2' } });
+  // }
+  next();
+});
+
+router.afterEach(function (to, from) {
+  console.log('Global AfterEach');
+  console.log(to, from);
+});
+export default router;
