bc7618fc2329e53429b3e1399837e41a31d42143
André Torquato <andretorquato@alu.ufc.br>
2022-01-05 22:30:21 -0300

    init vuex module

---
+++ vuex-01-starting-setup/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <base-container title="Vuex">
+    <h2>{{ $store.state.count }}</h2>
+    <button>Add</button>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+
+export default {
+  components: {
+    BaseContainer,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
