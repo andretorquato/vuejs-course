bde1fa9f02ca07f9912109692dfe4861ee3688ca
André Torquato <andretorquato@alu.ufc.br>
2022-01-26 23:30:58 -0300

    animation transition mode

--- animations-01-starting-setup/src/App.vue
+++ animations-01-starting-setup/src/App.vue
@@ -3,16 +3,22 @@
     <div class="block" :class="{ animation: isAnimate }"></div>
     <button @click="onAnimate">Animate</button>
   </div>
-   <div class="container">
+  <div class="container">
     <transition name="para">
       <p v-if="isVisibleParagraph">Show paragraph</p>
     </transition>
-     <button @click="showParagraph">Show</button>
-  </div>  
-    <base-modal @close="hideDialog" :open="dialogIsVisible">
-      <p>This is a test dialog!</p>
-      <button @click="hideDialog">Close it!</button>
-    </base-modal>
+    <button @click="showParagraph">Show</button>
+  </div>
+  <div class="container">
+    <transition name="fade-button" mode="out-in">
+    <button v-if="showUsers" @click="toggleUser">Show Users</button
+    ><button v-else @click="toggleUser">Hide Users</button>
+    </transition>
+  </div>
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
+    <p>This is a test dialog!</p>
+    <button @click="hideDialog">Close it!</button>
+  </base-modal>
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
   </div>
@@ -25,13 +31,17 @@ export default {
       isAnimate: false,
       isVisibleParagraph: false,
       dialogIsVisible: false,
+      showUsers: true,
     };
   },
   methods: {
+    toggleUser() {
+      this.showUsers = !this.showUsers;
+    },
     onAnimate() {
       this.isAnimate = !this.isAnimate;
     },
-    showParagraph(){
+    showParagraph() {
       this.isVisibleParagraph = !this.isVisibleParagraph;
     },
     showDialog() {
@@ -120,7 +130,7 @@ button:active {
 .para-enter-active {
   /* transition: transform 1s ease-in-out; */
 }
-.para-enter-from{
+.para-enter-from {
   /* transform: scale(1); */
 }
 .para-leave-from {
@@ -134,4 +144,24 @@ button:active {
   /* transform: scale(0.5); */
   animation: boo-peep 5s forwards;
 }
+
+.fade-button-enter-to{
+  opacity: 0;
+}
+.fade-button-enter-active {
+  transition: opacity 1s ease-in;
+}
+.fade-button-enter-from {
+  opacity: 1;
+}
+.fade-button-leave-to {
+  opacity: 1;
+}
+.fade-button-leave-from {
+  opacity: 0;
+}
+.fade-button-leave-active {
+  transition: opacity 1s ease-out;
+}
+
 </style>
