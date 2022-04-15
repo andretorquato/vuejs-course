8142fb33bea6113228c470ea580f4d64ecda7ddf
André Torquato <andretorquato@alu.ufc.br>
2022-04-15 20:57:38 -0300

    define reactive object with composition API

--- composition-assignment-1-problem/src/App.vue
+++ composition-assignment-1-problem/src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <h2>My Course Goal</h2>
+  <h2>{{ state.goal }}</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <h3 v-if="state.showGoal">OUTPUT COURSE GOAL</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button>Toggle Goal</button>
+  <button @click="changeGoal">Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
   <!-- => Separate refs -->
   <!-- => Ref Object -->
@@ -13,8 +13,18 @@
 </template>
 
 <script>
+import { reactive } from 'vue';
 export default {
-  
+  setup(){
+    const state = reactive({
+      goal: 'My Course Goal',
+      showGoal: true
+      });
+    function changeGoal () {
+      state.showGoal = !state.showGoal;
+    }
+    return { state, changeGoal };
+  }
 }
 </script>
 
