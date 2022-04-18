0fc59177ccc11cb143eb8a7f34c45b0cd3cbe387
André Torquato <andretorquato@alu.ufc.br>
2022-04-18 09:44:41 -0300

    finish challenge

--- composition-assignment-1-problem/src/App.vue
+++ composition-assignment-1-problem/src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <h2>{{ state.goal }}</h2>
+  <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3 v-if="state.showGoal">OUTPUT COURSE GOAL</h3>
+  <h3 v-if="showGoal">{{ goal }}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button @click="changeGoal">Toggle Goal</button>
+  <button @click="toggleChangeVisibilityGoal">Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
   <!-- => Separate refs -->
   <!-- => Ref Object -->
@@ -13,18 +13,46 @@
 </template>
 
 <script>
-import { reactive } from 'vue';
+// import { ref } from 'vue';
 export default {
-  setup(){
-    const state = reactive({
-      goal: 'My Course Goal',
-      showGoal: true
-      });
-    function changeGoal () {
-      state.showGoal = !state.showGoal;
+  // OPTIONS API
+  data(){
+    return {
+      goal: 'Learn Vue.js',
+      showGoal: true,
     }
-    return { state, changeGoal };
+  },
+  methods: {
+    toggleChangeVisibilityGoal(){
+      this.showGoal = !this.showGoal;
+    }
   }
+  // COMPOSITION API
+  // setup(){
+  //   // === REACTIVE OBJECT ===
+  //   // const state = reactive({
+  //   //   goal: 'My Course Goal',
+  //   //   showGoal: true
+  //   //   });
+
+  //   // === REF OBJECT ===
+  //   // const state = ref({
+  //   //   goal: 'Finish course',
+  //   //   showGoal: false
+  //   //   });
+  //   // === SEPARATE REFS ===
+  //   const showGoal = ref(false);
+  //   const goal = ref('Finish course');
+  //   function toggleChangeVisibilityGoal () {
+  //     // === REACTIVE OBJECT ===
+  //     // state.showGoal = !state.showGoal;
+  //     // === REF OBJECT ===
+  //     // state.value.showGoal = !state.value.showGoal;
+  //     // === SEPARATE REF ===
+  //     showGoal.value = !showGoal.value;
+  //   }
+  //   return { showGoal, goal, toggleChangeVisibilityGoal };
+  // }
 }
 </script>
 
